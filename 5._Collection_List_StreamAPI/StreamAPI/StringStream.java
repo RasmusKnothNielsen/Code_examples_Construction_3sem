9d832cde79c2c6f5678a3f3df81075735de6a74d
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2019-12-20 16:09:17 +0100

    Updated Plan

--- 5._Collection_List_StreamAPI/StreamAPI/StringStream.java
+++ 5._Collection_List_StreamAPI/StreamAPI/StringStream.java
@@ -9,24 +9,27 @@ public class StringStream {
     public static void main(String[] args) {
 
         // Create a Collection of words and print them out
-        Collection<String> words = Arrays.asList("The", "Brown", "", "fox", "Jumps", "The", "Lazy", "Dog");
+        Collection<String> words = Arrays.asList("The", "Brown", " ", "Fox", "Jumps", "Over", "The", "Lazy", "Dog");
         System.out.println(words);
 
-        // Save only nonEmpty entries from words and print them out
-        Collection<String> onlyWords = words.parallelStream().filter(word -> !word.isEmpty())
+        // Save only nonBlank entries from words and print them out
+        Collection<String> onlyWords = words.parallelStream()
+                .filter(word -> !word.isBlank())
                 .collect(Collectors.toList());
         System.out.println(onlyWords);
 
         // Join all entries into a string with space in between and add a period at the end
-        String sentence = words.stream().filter(word -> !word.isEmpty()).collect(Collectors.joining(" ")) + ".";
+        String sentence = words.stream()
+                .filter(word -> !word.isBlank())
+                .collect(Collectors.joining(" ")) + ".";
         System.out.println(sentence);
 
         // Count the number of words in the collection
-        long vocabulary = words.stream().filter(word -> !word.isEmpty()).count();
+        long vocabulary = words.stream().filter(word -> !word.isBlank()).count();
         System.out.println(vocabulary);
 
         // Count the number of distinct words in the collection
-        long distinctVocabulary = words.stream().filter(word -> !word.isEmpty()).distinct().count();
+        long distinctVocabulary = words.stream().filter(word -> !word.isBlank()).distinct().count();
         System.out.println(distinctVocabulary);
 
     }
--- Plan.md
+++ Plan.md
@@ -13,6 +13,7 @@ Done
 Done 
 
 - Collection (List) + StreamAPI
+Done
 
 - Threads
 
