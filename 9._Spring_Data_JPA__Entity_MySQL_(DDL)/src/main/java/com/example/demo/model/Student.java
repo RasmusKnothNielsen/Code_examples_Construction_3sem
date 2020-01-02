0d96705ccee41b18f57069ffded87488d295b157
Knud Billing <knudjbilling@gmail.com>
2020-01-02 13:45:23 +0100

    Update

--- 9._Spring_Data_JPA__Entity_MySQL_(DDL)/src/main/java/com/example/demo/model/Student.java
+++ 9._Spring_Data_JPA__Entity_MySQL_(DDL)/src/main/java/com/example/demo/model/Student.java
@@ -1,14 +1,15 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
 import java.util.Date;
 
 @Data
@@ -16,17 +17,20 @@ import java.util.Date;
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
 
     String name;
 
-    @Email(message="Should be an email.")
+    @Email(message = "Should be an email.")
     String email;
 
     @PastOrPresent
-    Date DOB;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern ="yyyy-MM-dd")
+    LocalDate dob;
 
     @ManyToOne
     @JoinColumn(name = "course_id")
     Course course;
+
 }
