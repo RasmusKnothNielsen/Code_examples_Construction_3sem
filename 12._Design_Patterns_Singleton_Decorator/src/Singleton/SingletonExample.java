4cf6ad509151d82ad613d39c0e1280c4bbf4ff17
Thomas Dahl <mail@thomasdahl.one>
2020-01-03 15:51:45 +0100

    Fixed small things

--- 12._Design_Patterns_Singleton_Decorator/src/Singleton/SingletonExample.java
+++ 12._Design_Patterns_Singleton_Decorator/src/Singleton/SingletonExample.java
@@ -7,45 +7,36 @@ public class SingletonExample {
     public static void main(String[] args) {
 
         // ----------------- Class singleton -----------------
+        System.out.println("Demonstrating Class-based singleton implementation");
 
         //ClassSingleton classSingleton = new ClassSingleton(); // The constructor ClassSingleton() is not visible
         ClassSingleton classSingleton1 = ClassSingleton.getInstance();
 
         System.out.println(classSingleton1.getInfo()); //Initial class info
 
+        //Attempting to create another instance, and setting the info to something different.
         ClassSingleton classSingleton2 = ClassSingleton.getInstance();
         classSingleton2.setInfo("New class info");
 
+        //Showing that the "two instances" are both changed - because they are the same instance.
         System.out.println(classSingleton1.getInfo()); //New class info
         System.out.println(classSingleton2.getInfo()); //New class info
 
+
         // ----------------- Enum singleton -----------------
+        System.out.println("\nDemonstrating Enum-based singleton implementation");
 
         EnumSingleton enumSingleton1 = EnumSingleton.INSTANCE.getInstance();
 
         System.out.println(enumSingleton1.getInfo()); //Initial enum info
 
+        //Attempting to create another instance, and setting the info to something different.
         EnumSingleton enumSingleton2 = EnumSingleton.INSTANCE.getInstance();
         enumSingleton2.setInfo("New enum info");
 
+        //Showing that the "two instances" are both changed - because they are the same instance.
         System.out.println(enumSingleton1.getInfo()); //New enum info
         System.out.println(enumSingleton2.getInfo()); //New enum info
-
-
-
-        //TODO - Slet dette når du har vist det til de andre?
-        System.out.println("\nExample of how Reflection can break the ClassSingleton:");
-        try {
-            Constructor constructor = classSingleton1.getClass().getDeclaredConstructor();
-            constructor.setAccessible(true);
-            ClassSingleton classSingleton = (ClassSingleton) constructor.newInstance();
-            classSingleton.setInfo("Oh no...");
-
-            System.out.println(classSingleton1.getInfo());
-            System.out.println(classSingleton.getInfo());
-
-        } catch (Exception ignored){}
-
 
     }
 
